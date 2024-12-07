e9b83439bbe4d9115682310b0496138f0b98323f
Abdullah <abdullahbola4@gmail.com>
2024-12-07 17:27:32 +0100

    The fizzbuzz game

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿void fizzBuzz()
+{
+    Console.Write("input a number : ");
+    int number = int.Parse(Console.ReadLine()!);
+
+    if (number % 2 ==0 && number % 3 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(number % 2 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if(number % 3 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(number);
+    }
+}
+
+fizzBuzz();
